caf7d6367f8d5ac05f7ffbab4ba56104d8f3c7c6
almahmoud <amahmou4@jhu.edu>
2019-05-29 17:02:25 -0400

    Changed to argparse

--- lib/galaxy/datatypes/converters/vcf_bgzip_to_vcf.py
+++ lib/galaxy/datatypes/converters/vcf_bgzip_to_vcf.py
@@ -5,15 +5,17 @@ Uses pysam to read a bgzip'd vcf file's contents and write them to an uncompress
 
 usage: %prog in_file out_file
 """
-import optparse
-
+import argparse
 import pysam
 
 
 def main():
-    parser = optparse.OptionParser()
-    (options, args) = parser.parse_args()
-    input_fname, output_fname = args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('output')
+    args = parser.parse_args()
+    input_fname = args.input
+    output_fname = args.output
 
     bgzipped = pysam.BGZFile(input_fname)
     with open(output_fname, 'w') as vcf_output:
