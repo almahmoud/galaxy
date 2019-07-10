f0aba60a53c79bfe65dea080db4a2b6d9778f680
vahid <jalili.vahid@gmail.com>
2019-07-09 22:49:56 -0700

    Refactor and extend user-objectstore integration tests.

--- test/integration/objectstore/test_plug_media.py
+++ test/integration/objectstore/test_plug_media.py
@@ -50,14 +50,32 @@ class BaseUserBasedObjectStoreTestCase(integration_util.IntegrationTestCase):
     def setUp(self):
         super(BaseUserBasedObjectStoreTestCase, self).setUp()
 
-    def run_tool(self, tool_id, history_id, inputs):
+    def run_tool(self, history_id):
+        hda1 = self.dataset_populator.new_dataset(history_id, content=TEST_INPUT_FILES_CONTENT)
+        self.dataset_populator.wait_for_history(history_id)
+        hda1_input = {"src": "hda", "id": hda1["id"]}
+        inputs = {
+            "input1": hda1_input,
+            "input2": hda1_input,
+        }
+
         self.dataset_populator.run_tool(
-            tool_id,
+            "create_10",
             inputs,
             history_id,
             assert_ok=True,
         )
         self.dataset_populator.wait_for_history(history_id)
+
+    @staticmethod
+    def assert_content(files, expected_content):
+        for filename in files:
+            with open(filename) as f:
+                content = f.read().strip()
+                assert content in expected_content
+                expected_content.remove(content)
+        # This confirms that no two (or more) files had same content.
+        assert len(expected_content) == 0
 
     @staticmethod
     def get_files_count(directory):
@@ -80,6 +98,7 @@ class DataPersistedOnUserMedia(BaseUserBasedObjectStoreTestCase):
 
     def setUp(self):
         super(DataPersistedOnUserMedia, self).setUp()
+        self.dataset_populator = DatasetPopulator(self.galaxy_interactor)
 
     def test_files_count_and_content_in_user_media(self):
         # This test check if tool execution results are correctly stored
@@ -104,16 +123,8 @@ class DataPersistedOnUserMedia(BaseUserBasedObjectStoreTestCase):
             # execution of any tool.
             assert self.get_files_count(plugged_media.get("path")) == 0
 
-            self.dataset_populator = DatasetPopulator(self.galaxy_interactor)
             with self.dataset_populator.test_history() as history_id:
-                hda1 = self.dataset_populator.new_dataset(history_id, content=TEST_INPUT_FILES_CONTENT)
-                self.dataset_populator.wait_for_history(history_id)
-                hda1_input = {"src": "hda", "id": hda1["id"]}
-                create_10_inputs = {
-                    "input1": hda1_input,
-                    "input2": hda1_input,
-                }
-                self.run_tool("create_10", history_id, create_10_inputs)
+                self.run_tool(history_id)
 
                 assert self.get_files_count(self.files_default_path) == 0
                 assert self.get_files_count(plugged_media.get("path")) == 11
@@ -121,22 +132,14 @@ class DataPersistedOnUserMedia(BaseUserBasedObjectStoreTestCase):
                 # Assert content
                 files = _get_datasets_files_in_path(plugged_media.get("path"))
                 expected_content = [str(x) for x in range(1, 11)] + [TEST_INPUT_FILES_CONTENT]
-                for filename in files:
-                    with open(filename) as f:
-                        content = f.read().strip()
-                        assert content in expected_content
-                        expected_content.remove(content)
-                # This confirms that no two (or more) files had same content.
-                assert len(expected_content) == 0
+                self.assert_content(files, expected_content)
 
                 history_details = self._get(path="histories/" + history_id)
                 datasets = json.loads(history_details.content)["state_ids"]["ok"]
 
                 assert len(datasets) == 11
 
-                data = {
-                    "purge": True
-                }
+                data = {"purge": True}
                 for dataset_id in datasets:
                     self._delete("histories/{}/contents/{}".format(history_id, dataset_id), data=data)
 
@@ -145,3 +148,33 @@ class DataPersistedOnUserMedia(BaseUserBasedObjectStoreTestCase):
                 # After purging, all the files in the user media should be deleted.
                 assert len(files) == 0
 
+    def test_anonymous_should_be_able_to_store_data_without_having_to_plug_a_media(self):
+        with self._different_user("vahid@test.com"):
+            # No file should be in the instance-wide storage before
+            # execution of any tool.
+            assert self.get_files_count(self.files_default_path) == 0
+
+            # self.dataset_populator = DatasetPopulator(self.galaxy_interactor)
+            with self.dataset_populator.test_history() as history_id:
+                self.run_tool(history_id)
+
+                assert self.get_files_count(self.files_default_path) == 11
+
+                # Assert content
+                files = _get_datasets_files_in_path(self.files_default_path)
+                expected_content = [str(x) for x in range(1, 11)] + [TEST_INPUT_FILES_CONTENT]
+                self.assert_content(files, expected_content)
+
+                history_details = self._get(path="histories/" + history_id)
+                datasets = json.loads(history_details.content)["state_ids"]["ok"]
+
+                assert len(datasets) == 11
+
+                data = {"purge": True}
+                for dataset_id in datasets:
+                    self._delete("histories/{}/contents/{}".format(history_id, dataset_id), data=data)
+
+                files = _get_datasets_files_in_path(self.files_default_path)
+
+                # After purging, all the files in the user media should be deleted.
+                assert len(files) == 0
